25c63c367ba65d252ce61c6d6b8919cd814626a4
Angelo Barcelos <angelorodrigues318@gmail.com>
2025-06-01 21:32:42 -0300

    movendo as coisas

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -10,25 +10,25 @@ export default function Banner() {
             id: 1,
             title: "The most comfortable chairs in the world",
             subTitle: "welcome to chairs",
-            image: "/src/assets/Banner/banner_image.png"
+            image: "public/assets/Banner/banner_image.png"
         },
         {
             id: 2,
             title: "The most comfortable chairs in the world",
             subTitle: "welcome to chairs",
-            image: "/src/assets/Banner/banner_image.png"
+            image: "public/assets/Banner/banner_image.png"
         },
         {
             id: 3,
             title: "The most comfortable chairs in the world",
             subTitle: "welcome to chairs",
-            image: "/src/assets/Banner/banner_image.png"
+            image: "public/assets/Banner/banner_image.png"
         },
         {
             id: 4,
             title: "The most comfortable chairs in the world",
             subTitle: "welcome to chairs",
-            image: "/src/assets/Banner/banner_image.png"
+            image: "public/assets/Banner/banner_image.png"
         },
     ]
 
